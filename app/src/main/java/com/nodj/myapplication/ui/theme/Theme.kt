e236dccdc5d81c3ccddd776f2961b559364732aa
Николай <raspaev.niko@mail.ru>
2023-10-05 14:32:20 +0400

    fix

--- app/src/main/java/com/nodj/myapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/nodj/myapplication/ui/theme/Theme.kt
@@ -20,6 +20,7 @@ private val DarkColorScheme = darkColorScheme(
     primary = Color.White,
     secondary = Color.Black,
     tertiary = Color.White,
+    onPrimary = Color.Black,
     //background = Color(0xFFFFFBFE)
 )
 
@@ -27,6 +28,7 @@ private val LightColorScheme = lightColorScheme(
     primary = Color.Black,
     secondary = Color.White,
     tertiary = Color.Black,
+    onPrimary = Color.White,
     //surface = Color.DarkGray,
     //background = Color.LightGray
     //onBackground = Color.LightGray
